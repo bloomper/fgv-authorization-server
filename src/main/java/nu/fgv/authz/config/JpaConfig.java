4fcacfcd4580a0b94119a755c1d07b3592a9b796
Anders Jacobsson <anders.jacobsson@columbusglobal.com>
2023-08-14 19:43:33 +0200

    Reverted UTC change

--- src/main/java/nu/fgv/authz/config/JpaConfig.java
+++ src/main/java/nu/fgv/authz/config/JpaConfig.java
@@ -1,22 +1,10 @@
 package nu.fgv.authz.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Optional;
 
 
 @Configuration
-@EnableJpaAuditing(dateTimeProviderRef = "utcDateTimeProvider")
+@EnableJpaAuditing
 public class JpaConfig {
-
-    @Bean
-    public DateTimeProvider utcDateTimeProvider() {
-        return () -> Optional.of(LocalDateTime.now(ZoneOffset.UTC));
-    }
-
 }
